594325461dc73a33da6f296b7cee6c5870f29a50
Nik Kantar <nik@nkantar.com>
2021-01-01 11:43:12 -0800

    Add support for multiple tracks per date

--- build.py
+++ build.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 
+from collections import defaultdict
 from pathlib import Path
 from shutil import copy, rmtree
 from string import Template
 
 
 SPLIT_CHAR = "|"
+INDEX_DATES = 7
 
 CURRENT_DIR_PATH = Path.cwd()
 INPUT_FILE_PATH = CURRENT_DIR_PATH / "input"
@@ -17,7 +19,7 @@ TEMPLATE_FILE_PATH = CURRENT_DIR_PATH / "template.html"
 ASSET_FILE_NAMES = ["normalize.css", "style.css"]
 
 
-POST_HTML = Template("<h2>$date</h2>$embed_html\n")
+POST_HTML = Template("<h2>$date</h2>$embeds_html\n")
 EMBED_HTML = Template(
     "<iframe src='https://open.spotify.com/embed/track/$track_id' "
     "width='300' height='380' frameborder='0' allowtransparency='true' "
@@ -25,25 +27,44 @@ EMBED_HTML = Template(
 )
 
 
-# "https://open.spotify.com/track/2FOPJIJvirxmqIVeAqeTx2?si=S7ieVaOoR7KcfiY_d8f1IA"
-# url.split('/')[-1].split('?')[0]
+def _ensure_track_id(track_id):
+    # https://open.spotify.com/track/7kPLrLCWRoevQlUY80I0nC?si=S7ieVaOoR7KcfiY_d8f1IA
+
+    # if URL, extract track ID
+    if track_id.startswith("https://"):
+        return track_id.split("/")[-1].split("?")[0]
+
+    # if not URL, assume track ID
+    return track_id
+
+
+def _posts_by_date(raw_lines):
+    posts = defaultdict(str)
+
+    for line in raw_lines:
+        date, track_id = line.split(SPLIT_CHAR)
+        track_id = _ensure_track_id(track_id)
+
+        embed_html = EMBED_HTML.substitute(track_id=track_id)
+
+        # reverse track order within each post
+        posts[date] = embed_html + posts[date]
+
+    return posts
 
 
 def generate_index():
     with open(INPUT_FILE_PATH) as input_file:
         raw_lines = input_file.read().splitlines()
 
-        sorted_raw_lines = sorted(raw_lines, reverse=True)
+        posts = _posts_by_date(raw_lines)
 
-        html = ""
-
-        for line in sorted_raw_lines:
-            date, track_id = line.split(SPLIT_CHAR)
-
-            embed_html = EMBED_HTML.substitute(track_id=track_id)
-            post = POST_HTML.substitute(date=date, embed_html=embed_html)
-
-            html += post
+        html = "".join(
+            [
+                POST_HTML.substitute(date=date, embeds_html=posts[date])
+                for date in list(reversed(posts.keys()))[:INDEX_DATES]
+            ]
+        )
 
         try:
             rmtree(OUTPUT_DIR_PATH)
