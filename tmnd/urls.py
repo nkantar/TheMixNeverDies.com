3f83841d42cc4038c63197190501d3a097e61d89
Nik Kantar <nik@nkantar.com>
2021-11-28 17:31:08 -0800

    Reorder URL includes

--- tmnd/urls.py
+++ tmnd/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
-    path("", include("tmnd.cms.urls")),
     path("", include("tmnd.main.urls")),
+    path("", include("tmnd.cms.urls")),
     path("django-rq/", include("django_rq.urls")),
 ]
